f8b4c6be32e99a33fcd5f11757b6c2a2a22ecc90
Tri Minh <leminhtri.nguyen@sjsu.edu>
2023-11-30 06:01:55 -0800

    ...

--- src/main/java/edu/sjsu/cs/CustomerProfileController.java
+++ src/main/java/edu/sjsu/cs/CustomerProfileController.java
@@ -1,11 +1,18 @@
 package edu.sjsu.cs;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class CustomerProfileController implements Initializable {
     @FXML 
@@ -22,6 +29,9 @@ public class CustomerProfileController implements Initializable {
 
     @FXML 
     Label phoneField;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -30,5 +40,13 @@ public class CustomerProfileController implements Initializable {
         emailField.setText(Main.getSession().getCurrentCustomer().getEmail());
         addressField.setText(Main.getSession().getCurrentCustomer().getAddress());
         phoneField.setText(Main.getSession().getCurrentCustomer().getPhone());
+    }
+
+    public void switchToCustomerMain(ActionEvent event) throws IOException {
+        this.root = (Parent) FXMLLoader.load(this.getClass().getResource("TestCustomerMainPage1.fxml"));
+        this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.scene = new Scene(this.root);
+        this.stage.setScene(this.scene);
+        this.stage.show();
     }
 }
